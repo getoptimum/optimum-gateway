3f22c35a45f549e1c79f973a692e18b30af840ee
Alejandro <abergasov@gmail.com>
2026-08-18 14:16:11 +0200

    log it

--- pkg/service/gossipsub-gateway/messages_proxy.go
+++ pkg/service/gossipsub-gateway/messages_proxy.go
@@ -71,7 +71,12 @@ func (s *Service) processCLAttestation(l logger.AppLogger, msg *entities.CLMessa
 	}
 
 	if !s.srvMsgRouter.ShouldForwardMessageToMumP2P(l, meta.Kind, msg.Topic, msg.Message) {
-		s.log.Info("attestation message rejected by router", logger.WithTopic(msg.Topic), logger.WithInt("size", len(msg.Message)))
+		s.log.Info("attestation message rejected by router",
+			logger.WithTopic(msg.Topic),
+			logger.WithInt("size", len(msg.Message)),
+			logger.WithBool("auth_enabled", s.authMgr.IsEnabled()),
+			logger.WithBool("has_valid_token", s.authMgr.HasValidToken()),
+		)
 		return
 	}
 	s.log.Info("processing attestation message from CL", logger.WithTopic(msg.Topic), logger.WithInt("size", len(msg.Message)))
