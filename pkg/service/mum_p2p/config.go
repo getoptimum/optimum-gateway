b1ad13fd5d7a38097b299c42991377700b5cba08
singhhp1069 <singhhp1069@gmail.com>
2026-09-23 12:01:40 +0300

    fix: coding gemotry mapping

--- pkg/service/mum_p2p/config.go
+++ pkg/service/mum_p2p/config.go
@@ -49,14 +49,14 @@ func (cfg *Config) Get() *commonentities.OptimumConfig {
 
 func toMumP2PConfig() *mump2pcfg.Config {
 	res := mump2pcfg.DefaultGossipSubConfig()
-	res.MeshD = 6
-	res.MeshDlo = 4
-	res.MeshDhi = 8
+	res.MeshD = 12
+	res.MeshDlo = 8
+	res.MeshDhi = 14
 	res.HeartbeatMS = 100
 	res.RLNC = mump2pcfg.RLNCConfig{
 		K:                  64,
-		MaxShardSize:       3300,
-		RedundancyFraction: 1.5,
+		MaxShardSize:       1600,
+		RedundancyFraction: 2.0,
 	}
 	return res
 }
